2ca9798eada3651d8f11e2e32386901dabccb92e
manojsraogithub <manojsraogithub@gmail.com>
2018-12-22 21:17:36 +0000

    gitpull1

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   response.end('Hello World1\n');
 }).listen(3536);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:3536/');
